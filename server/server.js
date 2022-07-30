505043181abd98b1b37eca1bc648fd50de2d983d
Akinade Mayowa <akinade.mayowa@gmail.com>
2022-07-30 05:16:52 +0100

    adding html pages to browsing

--- server/server.js
+++ server/server.js
@@ -1,8 +1,27 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
     //console.log('request made')
-    console.log(req.url, request.method)
+    //console.log(req.url, req.method)
+
+    // set header content type
+    res.setHeader('Content-Type', 'text/html')
+
+    // bad way to send response to browser
+    //res.write('<h2>Hello mayowa</h2>')
+    //res.end()
+
+    // send an html file
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err) {
+            console.log(err)
+            res.end()
+        } else {
+            res.write(data) // if responding only one item, use res.end(data)
+            res.end()
+        }
+    })
 })
 
 server.listen(3000, 'localhost', () => {
